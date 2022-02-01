66a6d5e9c7b2b89c82215e97390650f73661a0aa
Adham Atef <adham.atef45@gmail.com>
2022-02-01 21:20:10 +0200

    day-3 task is added

--- demolab2/LoginAuthApp/views.py
+++ demolab2/LoginAuthApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
@@ -10,40 +10,36 @@ from django.contrib import messages
 # Create your views here.
 
 
-
 def index(request):
-
     if request.method == 'POST':
         username = request.POST['username']
+        request.session['username'] = request.POST['username']
         password = request.POST['password']
 
-        user = auth.authenticate(username = username, password =password  )
+        user = auth.authenticate(username=username, password=password)
 
         if user is not None:
-            auth.login(request , user)
+            auth.login(request, user)
             return redirect('/home')
         else:
             messages.info(request, 'invalid username or password')
             return redirect("/")
     else:
-        return render(request,'index.html')
+        return render(request, 'index.html')
 
 
 def register(request):
-
     if request.method == 'POST':
-
         email = request.POST['email']
         username = request.POST['username']
-        password= request.POST['password']
+        password = request.POST['password']
 
-
-        user = User.objects.create_user(username = username , password = password , email = email)
+        user = User.objects.create_user(username=username, password=password, email=email, is_staff=True)
         user.save()
         print('user created')
         return redirect('/custom')
 
-    return render(request,'register.html')
+    return render(request, 'register.html')
 
 
 def custom(request):
--- demolab2/demolab2/settings.py
+++ demolab2/demolab2/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-f*yl7&k8=-qy0$%%^wqv3c1)dv6#)46eloaj8q=ieorkv@_v6u
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'demolab2.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -80,10 +77,9 @@ DATABASES = {
         'NAME': 'labtwo',
         'USER': 'postgres',
         'PASSWORD': 'adham8585',
-        'HOST':'localhost'
+        'HOST': 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -103,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -115,16 +110,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'assests')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assests')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
